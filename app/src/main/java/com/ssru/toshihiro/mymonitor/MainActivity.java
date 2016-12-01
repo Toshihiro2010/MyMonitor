61807aa88f78a0af44cff180f46a7eea50e10ba4
Toshihiro2010 <s56122201044@ssru.ac.th>
2016-12-01 21:12:27 +0700

    ทำ ListView จากฐานข้อมูลสำเร็จ

--- app/src/main/java/com/ssru/toshihiro/mymonitor/MainActivity.java
+++ app/src/main/java/com/ssru/toshihiro/mymonitor/MainActivity.java
@@ -1,17 +1,99 @@
 package com.ssru.toshihiro.mymonitor;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bindWidGet();
 
-
+        SynJson synJson = new SynJson();
+        synJson.execute();
 
     }
-}
+
+    private void bindWidGet() {
+        listView = (ListView) findViewById(R.id.listView);
+    }
+
+    private class SynJson extends AsyncTask<Void, Void, String> {
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                String strURL = "http://csclub.ssru.ac.th/s56122201044/csc4202/monitor/php_get_monitor.php";
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strURL).build();
+                Response response = okHttpClient.newCall(request).execute();
+                Log.d("Doin ==> ", "Sucress");
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("AsyncTask ==> ", e.toString());
+            }
+
+            return null;
+        }// doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                Log.d("01/12/59 ==> ", "SucessOnPost"+ s);
+                JSONArray jsonArray = new JSONArray(s);
+
+                final String[] iconString = new String[jsonArray.length()];
+                final String[] titleString = new String[jsonArray.length()];
+                final String[] priceString = new String[jsonArray.length()];
+                final String[] brandString = new String[jsonArray.length()];
+
+                for (int i = 0 ; i < jsonArray.length() ; i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    iconString[i] = jsonObject.getString("image");
+                    titleString[i] = jsonObject.getString("product_name");
+                    priceString[i] = jsonObject.getString("price");
+                    brandString[i] = jsonObject.getString("brand_name");
+
+                }
+                MyAdapter myAdapter = new MyAdapter(MainActivity.this, titleString, priceString, iconString);
+                listView.setAdapter(myAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(MainActivity.this, brandString[i] + " ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            } catch (Exception e) {
+                Log.d("01/12/59 ==> ", e.toString());
+
+            }
+
+
+        }
+    }//SynJSon
+}//Main
--- app/src/main/java/com/ssru/toshihiro/mymonitor/MyAdapter.java
+++ app/src/main/java/com/ssru/toshihiro/mymonitor/MyAdapter.java
@@ -1,9 +1,14 @@
 package com.ssru.toshihiro.mymonitor;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Admin on 01-Dec-16.
@@ -15,16 +20,16 @@ public class MyAdapter extends BaseAdapter {
     String[] strPrice;
     String[] strIcon;
 
-    public MyAdapter(Context context, String[] strPrice, String[] strTilte, String[] strIcon) {
+    public MyAdapter(Context context, String[] strTilte, String[] strPrice, String[] strIcon) {
         this.context = context;
-        this.strPrice = strPrice;
         this.strTilte = strTilte;
+        this.strPrice = strPrice;
         this.strIcon = strIcon;
     }
 
     @Override
     public int getCount() {
-        return strIcon.length;
+        return strTilte.length;
     }
 
     @Override
@@ -39,6 +44,20 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+        TextView txtTitle = (TextView) view1.findViewById(R.id.tvName);
+        txtTitle.setText(strTilte[i]);
+
+        TextView txtPrice = (TextView) view1.findViewById(R.id.tvPrice);
+        txtPrice.setText(strPrice[i]);
+
+        ImageView iconImage = (ImageView) view1.findViewById(R.id.tvIcon);
+        Picasso.with(context).load(strIcon[i]).resize(100, 100).into(iconImage);
+
+
+
+        return view1;
     }
 }
